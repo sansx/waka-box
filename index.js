a65bce1f056563cedf23402b10dd3c5d780f516b
sansx <xiathxth@gmail.com>
2021-03-04 14:27:06 +0800

    change format

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ async function updateGist(stats) {
     const line = [
       name.padEnd(11),
       time.padEnd(14),
-      generateBarChart(percent, 18), //21
+      generateBarChart(percent, 16), //21
       String(percent.toFixed(1)).padStart(5) + "%"
     ];
 
